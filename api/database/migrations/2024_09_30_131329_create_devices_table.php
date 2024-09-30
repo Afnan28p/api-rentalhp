f5723fadb239dfde47fd7fcdc538db09e71b5df3
Afnan28p <144474773+Afnan28p@users.noreply.github.com>
2024-09-30 20:18:00 +0700

    Membuat migrations

---
+++ api/database/migrations/2024_09_30_131329_create_devices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama',50);
+            $table->string('merek_device',50);
+            $table->string('tipe_device',50);
+            $table->double('harga_sewa_per_hari');
+            $table->integer('stock');
+            $table->string('status',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fakultas');
+    }
+};
---
+++ api/database/migrations/2024_09_30_131504_create_rentals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('device_id');
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->double('total_harga');
+            $table->string('status', 50);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('device_id')->references('id')->on('devices')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodis');
+    }
+};
